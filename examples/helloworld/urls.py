539f21455789c2490cae0d0d11de3b78d9700c58
Partho Bhowmick <p.s.bhowmick@gmail.com>
2021-09-05 18:06:49 -0500

    remove Django project

--- examples/helloworld/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import HelloWorldView, DataView
-
-urlpatterns = [
-    path('world/', HelloWorldView.as_view()),
-    path('data', DataView.as_view()),
-]
